3ad5e21d3bcba5b40d6cbe8fea3ccd589c734a87
Jairo Lopez Navarrete <12002138@student.pxl.be>
2026-09-17 03:48:16 +0200

    app echt een app maken op gsm

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'MaiBookStory',
+        short_name: 'MaiBookStory',
+        description:
+          'Jouw persoonlijke boekenwereld',
+        theme_color: '#f8f8f8',
+        background_color: '#f8f8f8',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+  {
+    src: '/maibookstory-icon.png',
+    sizes: '192x192',
+    type: 'image/png',
+  },
+  {
+    src: '/maibookstory-icon.png',
+    sizes: '512x512',
+    type: 'image/png',
+  },
+],
+      },
+
+      workbox: {
+        navigateFallback: '/',
+      },
+    }),
+  ],
 })
